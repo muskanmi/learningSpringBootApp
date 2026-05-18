d426d7f28cd3d5de804056c321e9492eec1bb090
Muskan Mittal <mittalmuskan758@gmail.com>
2026-05-18 12:50:51 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/codingshuttle/youtube/learningSpringBootApp/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.youtube.learningSpringBootApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/")
+    public String hello() {
+    return "Hello World";
+    }
+}
