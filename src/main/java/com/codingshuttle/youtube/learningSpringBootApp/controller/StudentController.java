794d9c1801e6e19c281c74f84f365c1bdfc31d70
Muskan Mittal <mittalmuskan758@gmail.com>
2026-05-21 14:58:20 +0530

    added springboot starter validation

--- src/main/java/com/codingshuttle/youtube/learningSpringBootApp/controller/StudentController.java
+++ src/main/java/com/codingshuttle/youtube/learningSpringBootApp/controller/StudentController.java
@@ -4,12 +4,15 @@ package com.codingshuttle.youtube.learningSpringBootApp.controller;
 import com.codingshuttle.youtube.learningSpringBootApp.dto.AddStudentRequestDto;
 import com.codingshuttle.youtube.learningSpringBootApp.dto.StudentDto;
 import com.codingshuttle.youtube.learningSpringBootApp.service.StudentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,8 +36,10 @@ public class StudentController {
     }
 
     @PostMapping
-    public ResponseEntity<StudentDto> createNewStudent(@RequestBody AddStudentRequestDto addStudentRequestDto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(studentService.createNewStudent(addStudentRequestDto));
+    public ResponseEntity<StudentDto> createNewStudent(@RequestBody @Valid AddStudentRequestDto addStudentRequestDto) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(studentService.createNewStudent(addStudentRequestDto));
     }
 
     @DeleteMapping("/{id}")
@@ -45,6 +50,16 @@ public class StudentController {
 
     @PutMapping("/{id}")
     public ResponseEntity<StudentDto> updateStudent(@PathVariable Long id, @RequestBody AddStudentRequestDto addStudentRequestDto) {
-        return ResponseEntity.status(HttpStatus.OK).body(studentService.updateStudent(id, addStudentRequestDto));
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(studentService.updateStudent(id, addStudentRequestDto));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<StudentDto> updatePartialStudent(@PathVariable Long id,
+                                                           @RequestBody Map<String, Object> updates) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(studentService.updatePartialStudent(id, updates));
     }
 }
--- src/main/java/com/codingshuttle/youtube/learningSpringBootApp/service/impl/StudentServiceImpl.java
+++ src/main/java/com/codingshuttle/youtube/learningSpringBootApp/service/impl/StudentServiceImpl.java
@@ -10,6 +10,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -50,6 +51,31 @@ public class StudentServiceImpl implements StudentService {
 
     @Override
     public StudentDto updateStudent(Long id, AddStudentRequestDto addStudentRequestDto) {
-        return null;
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Student not found with ID: "+id));
+        modelMapper.map(addStudentRequestDto, student);
+
+        student = studentRepository.save(student);
+        return modelMapper.map(student, StudentDto.class);
+    }
+
+    @Override
+    public StudentDto updatePartialStudent(Long id, Map<String, Object> updates) {
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Student not found with ID: "+id));
+        updates.forEach((field, value) -> {
+            switch (field) {
+                case "name":
+                    student.setName((String) value);
+                    break;
+                case "email":
+                    student.setEmail((String) value);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Field is not supported");
+            }
+        });
+        Student savedStudent = studentRepository.save(student);
+        return modelMapper.map(savedStudent, StudentDto.class);
     }
 }
